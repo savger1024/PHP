fc19653a680125161ad9fa5ca29ebb228c136701
savger1024 <90380477+savger1024@users.noreply.github.com>
2023-11-23 19:32:48 +0100

    PHP alapok (se)

---
+++ php/alap.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+php.net, 8.2, non thread safe, zip csomagolás, environment variable/környezeti változó változtatás,
+path, ctrlv, számítógép újraindítás, php --version
+int, float, bool, string, null, array (keverhető)
+echo (print is működik), var_dump, print_r, printf
+*/
+$num1 = 2;
+echo $num1;
+
+?>
